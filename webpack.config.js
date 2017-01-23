4aebd798c49e49ab5d558e0c0efcc57d0fadc5d3
dengjiayao <dnxbf321@gmail.com>
2017-01-23 16:02:10 +0800

    add

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * 自定义 webpack@2 任务配置
+ * 默认配置请查看 wcx/script/webpack-conf
+ * 配置项请参考：https://webpack.js.org/configuration/#options
+ * 可使用 process.env.NODE_ENV 区分 development、experiment、production 环境
+ * @type {Object}
+ */
+module.exports = {
+  module: {
+    rules: []
+  }
+}
